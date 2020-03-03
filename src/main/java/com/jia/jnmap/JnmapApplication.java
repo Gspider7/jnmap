4a94df983437a3f48278c08f89f8d3bfd7708f97
xutao@gaffassen.com <xutao@gaffassen.com>
2020-03-03 18:59:18 +0800

    init

---
+++ src/main/java/com/jia/jnmap/JnmapApplication.java
@@ -0,0 +1,13 @@
+package com.jia.jnmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JnmapApplication.class, args);
+    }
+
+}
