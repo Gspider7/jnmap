22bef1e39ba4b20f2cd6fc6e056adb4c2d2e1065
xutao@gaffassen.com <xutao@gaffassen.com>
2020-03-18 09:30:09 +0800

    init

--- src/main/java/com/jia/jnmap/config/InfinispanCacheConfig.java
+++ src/main/java/com/jia/jnmap/config/InfinispanCacheConfig.java
@@ -2,20 +2,21 @@ package com.jia.jnmap.config;
 
 import org.infinispan.Cache;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.cache.StorageType;
 import org.infinispan.manager.DefaultCacheManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * infinispan缓存配置
- * 参考：https://infinispan.org/documentation/
+ * 参考：
+ *      https://infinispan.org/docs/stable/titles/embedding/embedding.html
+ *      https://infinispan.org/docs/stable/titles/configuring/configuring.html#cache_configuration
  *
  * @author xutao@gaffassen.com
  * @version 1.0.0
  * @date 2020-03-16 11:47
  */
-@Configuration
 public class InfinispanCacheConfig {
 
     @Autowired
@@ -35,8 +36,9 @@ public class InfinispanCacheConfig {
 
         // infinispan本地缓存配置
         org.infinispan.configuration.cache.Configuration config = new ConfigurationBuilder()
-                .memory().size(1024)
-                .persistence().passivation(false).addSingleFileStore().shared(false).location(projectHome + "/infinispan")    // 使用本地存储
+                .memory().storageType(StorageType.OBJECT).size(1024)
+                .persistence().passivation(false)           // 持久化配置
+                .addSingleFileStore().shared(false).location(projectHome + "/infinispan")    // 使用本地存储
 //                .eviction().size(1024000).strategy(EvictionStrategy.LRU)                // 缓存淘汰机制：最少使用
                 .build();
 
